9860858ee1ff63d5a3f2d85669d5eb9d602dba25
JosephKevinMachado <joseph.machado.professional@gmail.com>
2024-08-15 09:50:15 -0400

    2024-08-15-09-50-15

--- data_pipeline_generator/_modidx.py
+++ data_pipeline_generator/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://josephmachado.github.io',
                 'git_url': 'https://github.com/josephmachado/data-pipeline-generator',
                 'lib_path': 'data_pipeline_generator'},
-  'syms': {'data_pipeline_generator.core': {'data_pipeline_generator.core.foo': ('core.html#foo', 'data_pipeline_generator/core.py')}}}
+  'syms': { 'data_pipeline_generator.core': { 'data_pipeline_generator.core.foo': ('core.html#foo', 'data_pipeline_generator/core.py'),
+                                              'data_pipeline_generator.core.say_hello': ( 'core.html#say_hello',
+                                                                                          'data_pipeline_generator/core.py')}}}
